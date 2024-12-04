260d71e7eca6c23219e8fdb324f108b65c069c0b
Hugo Lextrait <hugo.lextrait@gmail.com>
2024-12-04 14:48:38 +0100

    Change the display of daily rewards

--- src/components/element/token/Token.tsx
+++ src/components/element/token/Token.tsx
@@ -1,5 +1,5 @@
 import type { Token as TokenType } from "@merkl/api";
-import { Button, Dropdown, Icon, Value } from "packages/dappkit/src";
+import { Button, Dropdown, Icon, Text, Value } from "packages/dappkit/src";
 import { useMemo } from "react";
 import TokenTooltip from "./TokenTooltip";
 
@@ -12,18 +12,28 @@ export type TokenProps = {
 export default function Token({ token, amount, value }: TokenProps) {
   const display = useMemo(
     () => (
-      <>
-        {amount && <Value format="0.00a">{amount}</Value>} <Icon rounded size="sm" src={token.icon} />
-        {token.symbol}
-      </>
+      <Button look="soft">
+        <Icon rounded size="sm" src={token.icon} />
+        {amount && (
+          <Value format="0.00a" look={"bold"}>
+            {amount}
+          </Value>
+        )}
+        {<Text look={"bold"}>{token.symbol}</Text>}
+        {token?.price && amount && (
+          <Value look={"soft"} format="$0.00a">
+            {amount * token?.price}
+          </Value>
+        )}
+      </Button>
     ),
-    [token, amount],
+    [token, amount]
   );
 
   if (value) return display;
   return (
     <Dropdown content={<TokenTooltip {...{ token, amount }} />}>
-      <Button look="soft">{display}</Button>
+      {display}
     </Dropdown>
   );
 }
