a599a67bb4e799bbbb98778042b3253f51b5a199
indaviande <vianney@double.cat>
2024-12-05 15:28:37 +0100

    footer issue ok

--- src/routes/_merkl.tsx
+++ src/routes/_merkl.tsx
@@ -5,7 +5,7 @@ import Header from "src/components/layout/Header";
 
 export default function Index() {
   return (
-    <Group size="xl" className="!gap-0 min-h-screen flex-col">
+    <Group size="xl" className="!gap-0 min-h-screen !flex-nowrap flex-col">
       <Header />
       <main className="flex-1 h-full">
         <Outlet />
